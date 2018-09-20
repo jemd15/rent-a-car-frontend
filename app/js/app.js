e7c7a611a73c5f0e4a3e3ffbd0880dfaa7f44be9
JEMD <x_jemd_x@hotmail.com>
2018-09-20 13:31:42 -0300

    guardando cambios para borrar local

--- app/js/app.js
+++ app/js/app.js
@@ -8,11 +8,31 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('home', {
             url: "/home",
             templateUrl: "app/views/home.html",
-            controllerAs: 'HomeCtrl'
+            controller: 'HomeCtrl'
         })
         .state('login', {
             url: "/login",
             templateUrl: "app/views/login.html"
+        })
+        .state('drivers', {
+            url: "/drivers",
+            templateUrl: "app/views/drivers.html",
+            controller: "DriversCtrl"
+        })
+        .state('clients', {
+            url: "/clients",
+            templateUrl: "app/views/clients.html",
+            controller: "ClientsCtrl"
+        })
+        .state('rentACar', {
+            url: "/rentACar/:carID/",
+            templateUrl: "app/views/rent.html",
+            controller: "RentACarCtrl"
+        })
+        .state('rents', {
+            url: "/rents",
+            templateUrl: "app/views/rents.html",
+            controller: "RentsCtrl"
         })
 });
 
@@ -24,20 +44,33 @@ app.run(function($rootScope){
 
 app.factory('apiConnection', function ($http) {
 
-    var apiURL = 'http://localhost:8000/';
-    
+    //var apiURL = 'http://localhost:8100/';
+    var apiURL = 'http://10.170.224.24:3000/';
+
     var apiConnection = {
+
+        url: function () {
+            return apiURL
+        },
         
         getCars: function () {
-            return $http.get(apiURL + 'cars').then(function (data) {
+            return $http.get(apiURL + 'api/cars').then(function (data) {
+                return data
+            }, function (err) {
+                console.log('Error ' + err);
+            })
+        },
+
+        getCarsById: function (carId) {
+            return $http.get(apiURL + 'api/cars/' + carId).then(function (data) {
                 return data
             }, function (err) {
                 console.log('Error ' + err);
             })
         },
         
-        getUsers: function () {
-            return $http.get(apiURL + 'users').then(function (data) {
+        getClients: function () {
+            return $http.get(apiURL + 'api/users').then(function (data) {
                 return data
             }, function (err) {
                 console.log('Error ' + err);
@@ -45,11 +78,45 @@ app.factory('apiConnection', function ($http) {
         },
         
         getDrivers: function () {
-            return $http.get(apiURL + 'drivers').then(function (data) {
+            return $http.get(apiURL + 'api/drivers').then(function (data) {
                 return data
             }, function (err) {
                 console.log('Error ' + err);
             })
+        },
+
+        getRents: function () {
+            return $http.get(apiURL + 'api/rents').then(function (data) {
+                return data
+            }, function (err) {
+                console.log('Error ' + err);
+            })
+        },
+
+        postRent: function (firstname, lastname, telephone, rut, receptionDate, returnDate, email, guarantee, carID, driverID, totalPrice) {
+            var newRent = {
+                name: firstname,
+                lastname: lastname,
+                rut : rut,
+                telephone: telephone,
+                email: email,
+                guarantee: guarantee,
+                receptionDate: receptionDate,
+                returnDate: returnDate,
+                sumRent: totalPrice,
+                car: carID,
+                driver: driverID
+            };
+            $http.post(apiURL + 'api/unagreement', newRent).then(function () {
+                $('#rentModal').addClass('open');
+                console.log('open modal');
+            }, function (err) {
+                console.log('Error ' + err);
+            })
+        },
+
+        putCar: function (carID, carState) {
+
         }
         
     };
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,111 @@
-app.controller('HomeCtrl', function (apiConnection) {
-    var vm = this;
-    var apiUrl = 'http://localhost:8000/';
-    vm.cars = [];
+app.controller('HomeCtrl', function (apiConnection, $scope) {
     apiConnection.getCars().then(function (data) {
-        vm.cars = data.data;
+        $scope.apiUrl = apiConnection.url();
+        $scope.cars = data.data.cars;
+        //$scope.cars = data.data.cars;
     });
-    console.log(vm.cars);
+    $scope.filter = {
+        'type': '',
+        'state': ''
+    };
+});
+
+app.controller('DriversCtrl', function (apiConnection, $scope) {
+    apiConnection.getDrivers().then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.drivers = data.data.drivers; //.drivers;
+    });
+    $scope.filter= {
+        'gender': '',
+        "rate": ''
+    }
+});
+
+app.controller('ClientsCtrl', function (apiConnection, $scope) {
+    apiConnection.getClients().then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.clients = data.data.clients;
+    });
+    $scope.filter = {
+        'convenio': ''
+    }
+});
+
+app.controller('RentACarCtrl', function (apiConnection, $scope, $stateParams) {
+    apiConnection.getCarsById($stateParams.carID).then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.car = data.data.car;
+    });
+    $scope.convenio = 'sin-convenio';
+    apiConnection.getCars().then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.cars = data.data.cars;
+    });
+    apiConnection.getDrivers().then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.drivers = data.data.drivers;
+    });
+    $scope.car = {
+        id: $stateParams.carID,
+        price: $stateParams.carPrice,
+        img: $stateParams.carImg,
+        description: $stateParams.carDescription
+    };
+    $scope.rent = {
+        carID: '',
+        days: '',
+        client: '',
+        guarantee: '',
+        receptionDate: '',
+        returnDate: '',
+        totalAmount: '0'
+    };
+
+    $scope.openRentModal = function () {
+        $('#rentModal').addClass('open');
+    };
+
+    $scope.closeRentModal = function () {
+        $('#rentModal').removeClass('open');
+    };
+
+    $scope.makeRent = function (firstname, lastname, telephone, rut, receptionDate, returnDate, email, guarantee, carID, driverID, totalPrice) {
+        apiConnection.postRent(firstname, lastname, telephone, rut, receptionDate, returnDate, email, guarantee, carID, driverID, totalPrice)
+    };
+
+    $scope.iva = '19';
+
+    var currentTime = new Date();
+    $scope.currentTime = currentTime;
+    $scope.month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    $scope.monthShort = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    $scope.weekdaysFull = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    $scope.weekdaysLetter = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
+    //$scope.disable = [false, 1, 7];
+    $scope.today = 'Hoy';
+    $scope.clear = 'Borrar';
+    $scope.close = 'Cerrar';
+    var days = 30;
+    $scope.minDate = (new Date($scope.currentTime.getTime())).toISOString();
+    $scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
+
+    $scope.todayDate = currentTime.getDate() + "/" + (currentTime.getMonth()+1)  + "/" + currentTime.getFullYear();
+    var returnDate = $scope.rent.returnDate.split('/');
+    $scope.rent.days = $scope.rent.returnDate - $scope.todayDate;
+});
+
+app.controller('RentsCtrl', function (apiConnection, $scope) {
+    apiConnection.getRents().then(function (data) {
+        $scope.apiUrl = apiConnection.url();
+        $scope.rents = data.data.rents;
+    });
+    $scope.rent = {
+        carID: '',
+        days: '',
+        client: '',
+        guarantee: '',
+        receptionDate: '',
+        returnDate: '',
+        totalAmount: '0'
+    };
 });
